1502fcf3e1bd2f80ebfb190650f61dbd6e62909d
DESKTOP\Norm <norm.bosse0@gmail.com>
2020-02-21 13:42:10 -0500

    home3

---
+++ files/pages/home2.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOME2</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Euphoria+Script">
+    <link href="../css/home2.css" media="screen" rel="stylesheet" type="text/css">
+    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+    <link rel="shortcut icon" type="image/x-icon" href="../images/favicon.ico">
+
+</head>
+<body>
+
+    <main>
+        <section class="shawls grid">
+
+
+            <div class="box box1">1
+                <!-- <img class="shawl-image" src="../images/shawl_steel_11b.jpg"> -->
+            </div>
+            <div class="box box2">2
+            </div>
+            <div class="box box3">3
+            </div>
+            <div class="box box4">4
+            </div>
+            <div class="box box5">5
+            </div>
+            <div class="box box6">
+                <div class="text">Shawls</div>
+                <div class="text">&amp</div>
+                <div class="text">Scarves</div>
+            </div>
+            <div class="box box7">7
+            </div>
+            <div class="box box8">8
+            </div>
+            <div class="box box9">9
+            </div>
+            <div class="box box10">10
+            </div>
+            <div class="box box11">11
+            </div>
+            <div class="box box12">12
+            </div>
+            <div class="box box13">13
+            </div>
+        
+        
+        </section>
+        
+    
+    </main>
+    
+</body>
+</html>
